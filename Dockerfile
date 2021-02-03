c8ed4a46f9e059be3a3550db32595c105925fc0b
Alex Graber <ahgraber@ninerealmlabs.com>
2021-02-03 17:21:35 -0500

    docker secrets

--- Dockerfile
+++ Dockerfile
@@ -10,11 +10,4 @@ LABEL maintainer="Jamie Curnow <jc@jc21.com>"
 COPY 00_aria.cnf .
 RUN cat /00_aria.cnf >> /etc/mysql/my.cnf
 
-# secret-init.sh requires bash for variable expanshiopn
-RUN apk add --no-cache bash && \
-    rm -f /var/cache/apk/*
-
-COPY ./scripts/secret-init.sh /scripts/pre-init.d
-COPY ./scripts/secret-init.sh /scripts/
-# COPY ./scripts/01_run_secret-init.bash /scripts/pre-init.d
-COPY ./scripts/02_check-env.sh /scripts/pre-init.d
+COPY ./scripts/01_secret-init.sh /scripts/pre-init.d
--- scripts/01_secret-init.sh
+++ scripts/01_secret-init.sh
@@ -25,28 +25,31 @@ function setenv() { export "$1=$2"; }
 
 # iterate over environmental variables
 # if variable ends in "__FILE"
-for VARNAME in $(declare -p | grep "^.*__FILE="); do
+for FULLVAR in $(env | grep "^.*__FILE="); do
+    # trim "=..." from variable name
+    VARNAME=$(echo $FULLVAR | sed "s/=.*//g")
     log_note "[secret-init] evaluating ${VARNAME}"
 
     # set SECRETFILE to the contents of the variable
     # Use 'eval hack' for indirect expansion in sh: https://unix.stackexchange.com/questions/111618/indirect-variable-expansion-in-posix-as-done-in-bash
     # WARNING: It's not foolproof is an arbitrary command injection vulnerability 
-    eval SECRETFILE="\${${VARNAME}}"
+    eval SECRETFILE="\$${VARNAME}"
 
-    log_note "[secret-init] setting SECRETFILE to ${SECRETFILE}"  # DEBUG
+    # log_note "[secret-init] setting SECRETFILE to ${SECRETFILE}"  # DEBUG - rm for prod!
     
     # if SECRETFILE exists
     if [[ -f ${SECRETFILE} ]]; then
         # strip the appended "__FILE" from environmental variable name
-        # and set value to contents of secretfile
-        STRIPVAR=${VARNAME//__FILE/}
-        # log_note "[secret-init] set STRIPVAR to ${STRIPVAR}"  # DEBUG
-        eval ${STRIPVAR}=$(cat "${SECRETFILE}")
-        export "${STRIPVAR}"
-        # echo $(${STRIPVAR})
+        STRIPVAR=$(echo $VARNAME | sed "s/__FILE//g")
+        # log_note "[secret-init] set STRIPVAR to ${STRIPVAR}"  # DEBUG - rm for prod!
 
-        log_note "[secret_init] set ${STRIPVAR} to $(eval echo \${${STRIPVAR}})"  # DEBUG
+        # set value to contents of secretfile
+        eval ${STRIPVAR}=$(cat "${SECRETFILE}")
+        # log_note "[secret_init] set ${STRIPVAR} to $(eval echo \$${STRIPVAR})"  # DEBUG - rm for prod!
+        
+        export "${STRIPVAR}"
         log_note "[secret-init] ${STRIPVAR} set from ${VARNAME}"
+        
     else
         log_error "[secret-init] cannot find secret in ${VARNAME}"
     fi
